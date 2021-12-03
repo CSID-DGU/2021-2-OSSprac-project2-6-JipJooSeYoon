6330b20b9de54768f40fbb221511e8995af4007c
jyoon1421 <rose1421@naver.com>
2021-12-04 00:56:11 +0900

    Add 구매항목선택화면, 책사진파일

--- app/server.py
+++ app/server.py
@@ -13,41 +13,17 @@ def submit():
    card=request.form.get('card')
    return render_template("submit.html",name=name, address=address, card=card)
 
-@app.route('/detail', methods = ['POST', 'GET'])
-def detail():
+@app.route('/user_info', methods = ['POST', 'GET'])
+def user_info():
    if request.method == 'POST':
-      detail = dict()
-      detail['Name'] = request.form.get('Name')
-      detail['StudentNumber'] = request.form.get('StudentNumber')
-      detail['Gender'] = request.form.get('Gender')
-      detail['Major'] = request.form.get('Major')
-      return render_template("detail.html",detail = detail)
-
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = dict()
-      result['Name'] = request.form.get('Name')
-      result['StudentNumber'] = request.form.get('StudentNumber')
-      result['Gender'] = request.form.get('Gender')
-      result['Major'] = request.form.get('Major')
-      college = request.form.get('College')
-      if(college in ['불교대학', '문과대학', '법과대학', '사회과학대학', '경찰사법대학', '경영대학', '사범대학', '미래융합대학']): total = 6938000
-      elif(college in ['이과대학']): total = 8050000
-      elif(college in ['바이오시스템대학', '공과대학', '예술대학']): total = 9160000
-      elif(college in ['약학대학']): total = 11316000
-      result['Tuition'] = format(int(total * (1 - int(request.form.get('Scholarship'))/100)), ',d')
-      return render_template("result.html",result = result)
-
-@app.route('/calculate', methods = ['POST', 'GET'])
-def calculate():
-   if request.method == 'POST':
-      detail = dict()
-      detail['Name'] = request.form.get('Name')
-      detail['StudentNumber'] = request.form.get('StudentNumber')
-      detail['Gender'] = request.form.get('Gender')
-      detail['Major'] = request.form.get('Major')
-      return render_template('calculate.html',detail=detail)
+      book = request.form.get('book')
+      if(book in ['book5', 'book6']): cost = 17000
+      elif(book in ['book1']): cost = 15000
+      elif(book in ['book2']): cost = 14000
+      elif(book in ['book3']): cost = 15500
+      elif(book in ['book4']): cost = 14400
+      elif(book in ['book7']): cost = 14800
+      return render_template("user_info.html", cost=cost)
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
