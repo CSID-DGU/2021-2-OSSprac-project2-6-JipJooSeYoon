c13ee9543b5036bd7c5afd802ca8d91f720c695f
jibminJung <jibminjung@gmail.com>
2021-12-05 11:35:28 +0900

    결과 화면에 선택한 상품명  표시하도록 추가, 로그아웃 버튼 클릭시 초기화면으로 돌아가도록 추가

--- app/server.py
+++ app/server.py
@@ -8,10 +8,11 @@ def student():
 
 @app.route('/submit', methods = ['POST', 'GET'])
 def submit():
+   book = request.form.get('book')
    name=request.form.get('name')
    address=request.form.get('address')
    card=request.form.get('card')
-   return render_template("submit.html",name=name, address=address, card=card)
+   return render_template("submit.html",book=book,name=name, address=address, card=card)
 
 @app.route('/user_info', methods = ['POST', 'GET'])
 def user_info():
@@ -23,7 +24,22 @@ def user_info():
       elif(book in ['book3']): cost = 15500
       elif(book in ['book4']): cost = 14400
       elif(book in ['book7']): cost = 14800
-      return render_template("user_info.html", cost=cost)
+
+      book_title = books[book]
+
+      return render_template("user_info.html", cost=cost,book_title = book_title)
+
+@app.route('/select')
+def select_book():
+   return render_template("select.html")
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", debug=True, port=80)
+
+books = {'book1':"내 집 마련 가계부",
+'book2':"오리 집에 왜 왔니",
+'book3':"내 마음을 담은 집",
+'book4':"내 집 없는 부자는 없다",
+'book5':"지금이라도 집을 사야할까요?",
+'book6':"강남에 집 사고 싶어요",
+'book7':"노후를 위해 집을 이용하라"}
